459771ac21bc33726ef7236e816d2995b6620358
Chris Carlon <cmcarleone@gmail.com>
2024-10-08 18:25:43 +0100

    account for several geom columns, geom columns with different names

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use duckdb_load::launch_process_file;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     launch_process_file(
-        "/Users/cmcarlon/Downloads/designated-green-belt-land-borough.xls",
+        "/Users/cmcarlon/Downloads/osopenusrn_202410_gpkg/osopenusrn_202410.gpkg",
         "my_table",
     )?;
     Ok(())
