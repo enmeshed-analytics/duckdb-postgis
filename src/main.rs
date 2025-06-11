56e1af6f61b46808647992ea530aa99309947361
Chris Carlon <cmcarleone@gmail.com>
2025-06-11 22:40:11 +0100

    chore: main.rs typo [2025-06-11]

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use duckdb_load::core_processor::process_file;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     process_file(
-        "/Users/cmcarlon/Desktop/repdd/repd-q4-jan-2025.csv",
-        "repd_q4_jan_2025",
-        "postgresql://admin:password@localhost:5432/gridwalk",
-        "DESNZ",
+        "[insert file path here]",
+        "[insert table name here]",
+        "[insert postgres uri here]",
+        "[insert schema name here]",
     )?;
     Ok(())
 }
